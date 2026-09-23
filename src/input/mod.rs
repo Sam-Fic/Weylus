5f344c358f90783a211fb8f0038c36168075f6d1
Sam-Fic <samfic@users.noreply.github.com>
2026-09-24 03:55:00 +0800

    feat: Add XTest input device support as fallback for uinput
    Co-authored-by: icepie <icepie@users.noreply.github.com>
    (cherry picked from commit b42918b9)

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -9,5 +9,11 @@ pub mod uinput_device;
 #[allow(dead_code)]
 pub mod uinput_keys;
 
+#[cfg(target_os = "linux")]
+pub mod xtest_device;
+#[cfg(target_os = "linux")]
+#[allow(dead_code)]
+pub mod x11_keys;
+
 #[cfg(target_os = "macos")]
 mod macos_tablet;
